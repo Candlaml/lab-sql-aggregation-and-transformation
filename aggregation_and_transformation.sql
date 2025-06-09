d9139d3f0f0ead0d2391f955e7826c6dca30ad9d
Candela Medina <candela.medina@hotmail.co.uk>
2025-06-09 16:55:21 +0200

    Solved lab_2

--- aggregation_and_transformation.sql
+++ aggregation_and_transformation.sql
@@ -1,5 +1,6 @@
 -- Sakila Data Aggregation and Transformation
 -- You need to use SQL built-in functions to gain insights relating to the duration of movies:
+
 -- 1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
 SELECT MAX(length) AS max_duration, MIN(length) AS min_duration FROM film;
 
